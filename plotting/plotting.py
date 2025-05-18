88ea310c009b481ff741b63281b7a40ff49c175a
stecrotti <sky_96@live.it>
2025-05-18 13:24:49 +0200

    first commit

---
+++ plotting/plotting.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+
+df_un = pd.read_csv(cwd + '/../data/un/un_clean.csv')
+
+df = df_un[['Country', 'Population (millions)', 'Sex ratio (males to females)']]
+df = df_un[['Country', 'Population aged 0 to 14 years old (percentage)', 'Sex ratio (males to females)']]
+
+assert(len(df.columns) == 3)
+
+gpd_countries = gpd.read_file(cwd + '/gpd_countries.geojson')
+gpd_countries.reset_index(drop=True, inplace=True)
+print(type(gpd_countries))
+
+compatible_names = df['Country'].isin(gpd_countries['Country'])
+df = df[compatible_names]
+
+# fill with NaNs when countries in the map are not present in dataset
+countries_to_be_added = gpd_countries['Country'][~gpd_countries['Country'].isin(df['Country'])].to_frame()
+n = len(countries_to_be_added)
+countries_to_be_added.insert(1, df.columns.tolist()[1], np.nan * np.ones(n))
+countries_to_be_added.insert(1, df.columns.tolist()[2], np.nan * np.ones(n))
+
+df = pd.concat([df, countries_to_be_added])
+df.reset_index(drop=True, inplace=True)
+
+df_plot = gpd_countries.merge(df, on='Country')
+
+fig, axes = plt.subplots(2, 1)
+cmaps = ['Reds', 'Greens']
+
+for i in range(2):
+    feature = df_plot.columns[i+2]
+    df_plot.plot(
+        ax = axes[i],
+        column = df_plot[feature],
+        missing_kwds = {'color': 'lightgrey'},
+        cmap = cmaps[i],
+        legend = True
+    )
+    # fig.colorbar(axes[i].collections[0], ax = axes[i])
+
+    axes[i].set_xticks([])
+    axes[i].set_yticks([])
+
+    axes[i].title.set_text(feature)
+
+plt.show()
---
+++ plotting/save_gpd_countries.py
@@ -0,0 +1,11 @@
+"Donwload and save in a .geojson file the GeoDataFrame necessary for plotting"
+
+import geopandas as gpd
+import os
+
+url = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip"
+gpd_countries = gpd.read_file(url)
+gpd_countries.rename(columns={'NAME': 'Country'}, inplace=True)
+gpd_countries = gpd_countries[['Country', 'geometry']]
+
+gpd_countries.to_file(os.path.dirname(os.path.realpath(__file__)) + '/gpd_countries.geojson')
