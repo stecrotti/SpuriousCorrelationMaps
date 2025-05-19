a010a017ae1a44b79c6ec2575df5147fdb6de43e
stecrotti <sky_96@live.it>
2025-05-19 22:04:23 +0200

    smaller plot

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ from make_dataset import make_and_save_dataset
 import os
 import geopandas as gpd
 import random
+import matplotlib
+
+st.set_page_config(
+        page_title = "Spurious Correlation Maps",
+        page_icon = ":fried_shrimp:",
+        layout="wide",
+    )
 
 st.title('Spurious Correlation Maps')
 
@@ -17,6 +24,13 @@ if st.button("Generate random pair of maps", type="primary"):
 
     valid_features = df.columns[~df.columns.isin(['Country', 'geometry'])].to_list()
     name1, name2 = random.sample(valid_features, 2)
+    # matplotlib.rcParams.update({'font.size': 6})
     fig = plotting.plot_two_series(df, name1, name2)
-    st.write(fig)
+    # fig.set_size_inches(3, 3)
+
+    colsize = 0.4
+    _side = (1 - colsize) / 2
+    col1, col2, _ = st.columns([_side, colsize, _side])
+    with col2:
+        st.pyplot(fig)
 
